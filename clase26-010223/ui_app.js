f6c91963b9950d09c727581865ae3706a4bc3ca4
Gaston Avila <gastonavila14@gmail.com>
2023-02-04 13:19:14 -0300

    Validar solo con mensaje pop up (Toast)

--- clase26-010223/ui_app.js
+++ clase26-010223/ui_app.js
@@ -3,29 +3,47 @@ let inputSessionNro = document.getElementById('sesionNro')
 let inputArtist = document.getElementById('artist')
 let inputYoutubeVideo = document.getElementById('youtube')
 let listItemsHTML = document.getElementById('playlist_items')
+const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveSuccess = document.getElementById('liveToastSuccess')
+const toastLiveError = document.getElementById('liveToastError')
 
 let playlist = []
 
 formSongHTML.onsubmit = (e)=>{
+    const formOk = formIsValid();
     e.preventDefault()
-    let song = {
-        vol: inputSessionNro.value,
-        artista: inputArtist.value,
-        youtube: inputYoutubeVideo.value
+    if(formOk){
+        let song = {
+            vol: inputSessionNro.value.trim(),
+            artista: inputArtist.value.trim(),
+            youtube: inputYoutubeVideo.value.trim()
+        }
+        if(song.youtube){ // pregunto si tiene un video de youtube cargado
+            listItemsHTML.innerHTML += `<li class="list-group-item">Sesion nro: ${song.vol} - Artista: ${song.artista} - <a target="_blank" href="${song.youtube}">Ver vídeo</a></li>`
+        }else{
+            listItemsHTML.innerHTML += `<li class="list-group-item">Sesion nro: ${song.vol} - Artista: ${song.artista}</li>`
+        }
+        playlist.push(song)
+        formSongHTML.reset()
     }
-    playlist.push(song)
-
-    listItemsHTML.innerHTML += `<li class="list-group-item">Sesion nro: ${inputSessionNro.value} - Artista: ${inputArtist.value} - Youtube link: ${inputYoutubeVideo.value}</li>`
-    formSongHTML.reset()
 }
 
+function formIsValid(){
+    let isValid = true;
+    if(inputSessionNro.value.trim() == "" || inputArtist.value.trim() == ""){
+        isValid = false;
+    }
+    return isValid;
+}
 
-const toastTrigger = document.getElementById('liveToastBtn')
-const toastLiveExample = document.getElementById('liveToast')
-if (toastTrigger) {
-  toastTrigger.addEventListener('click', () => {
-    const toast = new bootstrap.Toast(toastLiveExample)
-
+toastTrigger.onclick = ()=>{
+    const formOk = formIsValid();
+    let toast = null;
+    if(formOk){
+        toast = new bootstrap.Toast(toastLiveSuccess)
+    }
+    else{
+        toast = new bootstrap.Toast(toastLiveError)
+    }
     toast.show()
-  })
 }
