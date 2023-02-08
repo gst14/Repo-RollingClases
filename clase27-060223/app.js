2bc8a8aa9af81e090327334b8a4bce7b39b9b0c4
Gaston Avila <gastonavila14@gmail.com>
2023-02-08 17:54:35 -0300

    Clase 27 - Realizado en clase

---
+++ clase27-060223/app.js
@@ -0,0 +1,21 @@
+let songObjetoLiteral = {
+    vol: "53",
+    artista: "Shakira",
+    youtube: "",
+    fechaLanzamiento: "01/01/2023",
+    producer: {
+        nombreArtistico: "Bizarrap",
+        nombreReal: "Gonzalo Julián Conde"
+    }
+}
+
+let songJSON = `{
+    "vol": "53",
+    "artista": "Shakira",
+    "youtube": "",
+    "producer": {
+        "nombreArtistico": "Bizarrap",
+        "nombreReal": "Gonzalo Julián Conde"
+    }
+}`
+
