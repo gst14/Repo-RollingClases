4730d5cb481699af35880a701f83b51ce011b1a5
Gaston Avila <gastonavila14@gmail.com>
2023-02-13 19:20:41 -0300

    Clase 28 - Reto parte 1: Solucion tarea

--- clase28-reto-js/js/shopping.js
+++ clase28-reto-js/js/shopping.js
@@ -1,8 +1,25 @@
 let shoppingCartItems = []
 let cartCountDOM = document.getElementById('items-shop')
-const storageShoppingCartItems = localStorage.getItem('shoppingCartItems')
-if (storageShoppingCartItems) {
+let listItemsShoppingDOM = document.getElementById('list-items')
+const storageShoppingCartItems = localStorage.getItem('shopping') // null
+
+if (storageShoppingCartItems) { // storageShoppingCartItems es distinto de null
     shoppingCartItems = JSON.parse(storageShoppingCartItems)
 }
-localStorage.setItem('shoppingCartItems')
 cartCountDOM.textContent = shoppingCartItems.length
+
+// alert(`Hay ${shoppingCartItems.length} items en el carrito`)
+
+let liLISTHtml = []
+for (let i = 0; i < shoppingCartItems.length; i++) {
+    const item = shoppingCartItems[i];
+    const liHTML = `<li class="list-group-item d-flex align-items-center">
+                        <span class="badge bg-primary custom-color rounded-pill">1</span>
+                            ${item.nombre}
+                    </li>`
+    liLISTHtml.push(liHTML)
+}
+
+
+listItemsShoppingDOM.innerHTML = liLISTHtml.join('')
+
